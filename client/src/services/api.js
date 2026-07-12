632c4c203b68c459bfcf24aa0e08c1c041203ceb
Mariyam2724 <85488366+Mariyam2724@users.noreply.github.com>
2026-07-13 01:52:53 +0530

    Fix production API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-management-app-csd3.onrender.com",
+  baseURL: "https://task-management-app-csd3.onrender.com/api",
 });
 
 export default API;
